aee94d9e4b7a4a0593f8bd0b02a3b7aaa1f931f7
Jhonatan de Oliveira Silva <jhonatandevbr@gmail.com>
2024-02-15 09:52:28 +0900

    SinstemCoins
    coin collection system

---
+++ Scripts/Coins/RotateCoin.lua
@@ -0,0 +1,4 @@
+while true do 
+    wait(0.002)
+    script.Parent.CFrame = script.Parent.CFrame * CFrame.fromEulerAnglesXYZ(0,0.01,0)
+    end 
---
+++ Scripts/Coins/Sounds.lua
@@ -0,0 +1,21 @@
+-- Variavel tempo para habilitar a som 
+local COOLDOWN = 10
+
+-- Variavel volume para controlar o volume do som
+local volume = game.Workspace.WordCoins.Coins.Coin.Sound
+volume.Volume=("0.070") -- ajuste aqui
+
+
+-- funcao para ativar o som ao tocar na moeda 
+function OnTouched(hit)
+	
+	if hit.Parent:FindFirstChild('Humanoid') then
+		if script.Parent.Transparency * 1 then -- se a transparencia for 1
+		script.Parent.Sound:Play()             -- de play no som 
+			script.Parent.SoundS.Disabled = true -- e desative o som 
+			task.wait(COOLDOWN)                   -- aguarde 10 segundos
+			script.Parent.SoundS.Disabled = false -- e ative o som
+		end
+	end
+end
+script.Parent.Touched:Connect(OnTouched)
